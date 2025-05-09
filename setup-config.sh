829a9e6b459c4bc8425cbe2a57767aaa975b57a4
Hai <thanhhai.fu@gmail.com>
2025-05-09 09:37:25 +0700

    update config file

--- setup-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy template files to actual configuration files
-cp src/main/resources/application.yml.template src/main/resources/application.yml
-cp src/main/resources/application-dev.yml.template src/main/resources/application-dev.yml
-cp src/main/resources/application-prod.yml.template src/main/resources/application-prod.yml
-
-echo "Configuration files have been created. Please update the credentials and settings as needed."
